dad1cf5614e4ba4a069b887bed19c44c97d4be19
Tim B. Herbstrith <tim.benedikt.herbstrith@univie.ac.at>
2017-02-08 12:54:10 +0100

    changed Stack to [Float]

--- halc.hs
+++ halc.hs
@@ -1,82 +1,55 @@
 import System.IO
 
-data Stack a = EmptyStack | Push a (Stack a)
-    deriving (Read, Eq, Ord) 
+type Stack = [Float]
 
-instance (Show a) => Show (Stack a) where
-    show = (++ " <-") . show . reverse . toList 
+main = calc []
 
-pop :: Stack b -> (Maybe b, Stack b)
-pop  EmptyStack = (Nothing, EmptyStack)
-pop (Push x stack) = (Just x, stack)
-
-popN :: (Integral a, Eq b) => a -> Stack b -> 
-                              (Maybe [b], Stack b)
-popN _ EmptyStack = (Nothing, EmptyStack)
-popN 1 (Push x stack) = (Just [x], stack)
-popN n (Push x stack)
-    | n < 1 = error "Can't return less than one item"
-    | otherwise = if may == Nothing
-                  then (Nothing, EmptyStack)
-                  else let (Just xs) = may
-                       in (Just (x:xs), stack')
-    where (may, stack') = popN (n - 1) stack
-
-push :: b -> Stack b -> Stack b
-push x stack = Push x stack
-
-showFst :: (Show b) => Stack b -> String
-showFst EmptyStack = "Nothing"
-showFst (Push x _) = show x
-
-toList :: Stack b -> [b]
-toList EmptyStack = []
-toList (Push x stack) = x:toList stack
-
-
-main = calc EmptyStack
-
-calc :: Stack Float -> IO ()
+calc :: Stack -> IO ()
 calc stack = do
     putStr "In: "
     hFlush stdout
     comm <- getLine
     if comm == "show"
         then do
-            print stack
+            putStrLn $ show (reverse stack) ++ " <-"
             calc stack 
         else do
-            stack' <- getStack stack $ parsing (Just stack) $ words comm
+            stack' <- getStack stack $ 
+                            parsing (Just stack) $ 
+                            words comm
             putStr "Out: "
-            putStrLn $ showFst stack'
+            putStrLn $ showHead stack'
             calc stack'
 
-getStack :: (Eq a) => Stack a -> Maybe (Stack a) -> 
-                      IO (Stack a)
+showHead :: Stack -> String
+showHead [] = "Nothing"
+showHead (x:stack) = show x
+
+getStack :: Stack -> Maybe Stack -> IO Stack
 getStack stack Nothing = do 
     putStrLn "Can't perform operation"
     return stack
 getStack _ (Just stack') = return stack'
 
-parsing :: Maybe (Stack Float) -> [String] -> Maybe (Stack Float)
+parsing :: Maybe Stack -> [String] -> Maybe Stack
 parsing (Just stack) [] = Just stack
 parsing Nothing _ = Nothing
 parsing (Just stack) (c:cs) = parsing stack' cs
     where stack' = pars stack c
 
-pars :: Stack Float -> String -> Maybe (Stack Float)
-pars (Push x (Push y stack)) "+" = Just $ push (y + x) stack
-pars (Push x (Push y stack)) "-" = Just $ push (y - x) stack
-pars (Push x (Push y stack)) "*" = Just $ push (y * x) stack
-pars (Push x (Push y stack)) "/" = Just $ push (y / x) stack
-pars (Push x (Push y stack)) "^" = Just $ push (y ** x) stack
-pars (Push x stack) "!" = Just $ push (fak x) stack
-pars (Push x stack) "abs" = Just $ push (abs x) stack
-pars stack "sum" = Just $ push (sum $ toList stack) EmptyStack
-pars (Push x (Push y stack)) "swp" = Just $ push y (push x stack)
-pars _ "clr" = Just EmptyStack
+pars :: Stack -> String -> Maybe Stack
+pars (x:y:stack) "+" = Just $ (y + x) : stack
+pars (x:y:stack) "-" = Just $ (y - x) : stack
+pars (x:y:stack) "*" = Just $ (y * x) : stack
+pars (x:y:stack) "/" = Just $ (y / x) : stack
+pars (x:y:stack) "^" = Just $ (y ** x) : stack
+pars (x:stack) "!" = Just $ (fak x) : stack
+pars (x:stack) "abs" = Just $ (abs x) : stack
+pars stack "sum" = Just $ (sum stack) : []
+pars (x:y:stack) "swp" = Just $ y : x : stack
+pars _ "clr" = Just []
 pars stack comm
-    | contNum comm = Just $ push (read comm :: Float) stack
+    | contNum comm = Just $ (read comm) : stack
     | otherwise = Nothing
 
 contNum :: String -> Bool
@@ -88,4 +61,4 @@ fak x
     | x' == 1.0 = 1.0
     | x < 0 = error "Negativ factorial"
     | otherwise = x' * (fak $ x' - 1.0)
-    where x' = (fromIntegral (truncate x :: Int) :: Float)
+    where x' = fromIntegral (truncate x :: Int)
